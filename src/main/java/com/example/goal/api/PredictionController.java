7e985c422e5b499b50a819767c3576d404dc8202
TejPatel11 <tejpatel798@gmail.com>
2022-08-09 11:31:23 -0400

    CRUD

---
+++ src/main/java/com/example/goal/api/PredictionController.java
@@ -0,0 +1,39 @@
+package com.example.goal.api;
+
+import com.example.goal.model.Prediction;
+import com.example.goal.service.PredictionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "predictions/")
+public class PredictionController {
+    private final PredictionService predictionService;
+
+    @Autowired
+    public PredictionController(PredictionService predictionService) {
+        this.predictionService = predictionService;
+    }
+
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    int insertPrediction(@RequestBody Prediction prediction){
+        return predictionService.insertPrediction(prediction);
+    }
+    @PutMapping(value = "{id}")
+    int updatePrediction(@PathVariable("id")int id, @RequestBody Prediction prediction){
+        return predictionService.updatePrediction(id,prediction);
+    }
+
+    @DeleteMapping(value = "{id}")
+    int deletePrediction(@PathVariable("id") int id){
+        return predictionService.deletePrediction(id);
+    }
+
+    @GetMapping
+    List<Prediction> queryPrediction(){
+        return predictionService.queryPrediction();
+    }
+}
---
+++ src/main/java/com/example/goal/service/PredictionService.java
@@ -0,0 +1,36 @@
+package com.example.goal.service;
+
+import com.example.goal.dao.PredictionDao;
+import com.example.goal.model.Prediction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PredictionService {
+    private final PredictionDao predictionDao;
+
+    @Autowired
+    public PredictionService(PredictionDao predictionDao) {
+        this.predictionDao = predictionDao;
+    }
+
+    public int insertPrediction(Prediction prediction){
+        return predictionDao.insertPrediction(prediction);
+    }
+    public int updatePrediction(int id, Prediction prediction){
+        return predictionDao.updatePrediction(id,prediction);
+    }
+    public int deletePrediction(int id){
+        return predictionDao.deletePrediction(id);
+    }
+    public List<Prediction> queryPrediction(){
+        return predictionDao.queyPrediction();
+    }
+    public Optional<Prediction> queryPrediction(int id){
+        return predictionDao.queryPrediction(id);
+    }
+}
